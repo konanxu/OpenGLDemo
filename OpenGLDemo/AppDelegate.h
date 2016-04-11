14d4cdc309180980a2c03996d4eb488eede0baf3
konanxu <konanxuxi@gmail.com>
2016-04-11 10:27:09 +0800

    Initial Commit

---
+++ OpenGLDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLDemo
+//
+//  Created by Konan on 16/4/11.
+//  Copyright © 2016年 Konan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLDemo
+//
+//  Created by Konan on 16/4/11.
+//  Copyright © 2016年 Konan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
